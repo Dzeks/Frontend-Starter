12cfccac05ab63acb2afa97938f2932ecf8d3d23
Evgeny Dmitriev <evgeny.dmitriev@coople.com>
2021-05-22 13:22:14 +0300

    List admin products

---
+++ src/app/admin-page/product-list/product-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from "@angular/core";
+import { SelectionModel } from "@angular/cdk/collections";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ApiService } from "../../domain/api.service";
+import { ProductDto } from "../../domain/api.types";
+import { MatSelectionListChange } from "@angular/material/list";
+import { MatCheckboxChange } from "@angular/material/checkbox";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.scss"],
+})
+export class ProductListComponent implements OnInit {
+  items: ProductDto[] = [];
+  selection = new SelectionModel<number>(true);
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar) {}
+  ngOnInit() {
+    this.apiService
+      .getProducts({ _page: 0, _limit: 1000 })
+      .then((items) => (this.items = items));
+  }
+  onSelectionChange(selection: MatSelectionListChange) {
+    selection.option.selected
+      ? this.selection.select(selection.option.value)
+      : this.selection.deselect(selection.option.value);
+  }
+  onRemove($event: MouseEvent, index: number, item: ProductDto) {
+    $event.stopPropagation();
+    this.apiService.deleteProduct(item.id).then(() => {
+      this.items.splice(index, 1);
+      this.selection.deselect(item.id);
+      this.items = [...this.items];
+    });
+  }
+  trackByFn(index: number, item: ProductDto) {
+    return item.id;
+  }
+  onEdit($event: MouseEvent) {
+    $event.stopPropagation();
+  }
+  onRemoveAll() {
+    this.apiService
+      .deleteProducts(this.selection.selected)
+      .then(() => {
+        this.items = this.items.filter(
+          (item) => !this.selection.isSelected(item.id)
+        );
+        this.selection.clear();
+      })
+      .catch((error) => this.snackBar.open(error, "", { duration: 2000 }));
+  }
+
+  onSelectAll($event: MatCheckboxChange) {
+    $event.checked
+      ? this.selection.select(...this.items.map((item) => item.id))
+      : this.selection.clear();
+  }
+}
---
+++ src/app/admin-page/product-overlay/product-overlay.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+
+@Component({
+  selector: "app-product-overlay",
+  templateUrl: "./product-overlay.component.html",
+  styleUrls: ["./product-overlay.component.scss"],
+})
+export class ProductOverlayComponent implements OnInit {
+  @ViewChild("overlayTpl", { static: false }) overlayTpl!: TemplateRef<any>;
+  private dialogRef?: MatDialogRef<unknown, any>;
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  editProduct($event: MouseEvent) {
+    this.dialogRef = this.dialog.open(this.overlayTpl, {
+      width: "250px",
+      data: {},
+    });
+  }
+}
